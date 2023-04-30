02da87cf74aa4dfefd0448b8590d5a6596e916d8
kamaleshwaran2002 <ks110302@gmail.com>
2023-04-30 13:04:06 +0530

    adding function

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -2,6 +2,8 @@ const cell = document.querySelectorAll(".cell")
 const statusText = document.querySelector("#statusText")
 const restartBtn = document.querySelector("#restartBtn")
 
+console.log(cell)
+
 const winConditions = [
     // rows
     [0,3,6],
@@ -16,3 +18,51 @@ const winConditions = [
     [6,4,2],
 
 ];
+
+let options = ["", "", "", "", "", "", "", "",""]
+let currentPlayer = "X"
+let running = false
+
+intializeGame()
+function intializeGame(){
+    cell.forEach(cell => cell.addEventListener("click", cellClicked))
+    restartBtn.addEventListener("click", restartGame)
+    statusText.textContent = `${currentPlayer}'s turn` 
+    running = true
+}
+// console.log(running)
+function cellClicked(){
+    // console.log("clicked")
+    const cellIndex = this.getAttribute("cellIndex")
+    console.log(cellIndex)
+    // cellIndex = 0 , 1, 2
+    if(options[cellIndex] != "" || !running){
+        console.log("hii")
+        return;
+    }
+
+    updateCell(this,cellIndex)
+    changePlayer()
+    checkWinner()
+    
+}
+
+function updateCell(cell, index){
+    options[index] = currentPlayer
+    cell.textContent = currentPlayer
+    // console.log("updatecell")
+}
+
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X" ) ? "O" : "X"
+    statusText.textContent = `${currentPlayer}'s turn`
+}
+
+function checkWinner(){
+    console.log("checkwinner")
+
+}
+
+function restartGame(){
+    console.log("restart")
+}
