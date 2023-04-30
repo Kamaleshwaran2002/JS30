ab9b60a58427854d4ab598c6562c732e25d6a002
kamaleshwaran2002 <ks110302@gmail.com>
2023-04-30 11:00:58 +0530

    declaring winning condition

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -1,3 +1,18 @@
 const cell = document.querySelectorAll(".cell")
 const statusText = document.querySelector("#statusText")
 const restartBtn = document.querySelector("#restartBtn")
+
+const winConditions = [
+    // rows
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    // columms
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    // diagonals
+    [0,4,8],
+    [6,4,2],
+
+];
